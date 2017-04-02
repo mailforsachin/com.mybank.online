b7eaffe6b4b518bd551c4f097d93c69b9d10591f
mailforsachin <mailforsachin@gmail.com>
2017-04-02 12:04:07 +0530

    Created MyScript.groovy

---
+++ MyScript.groovy
@@ -0,0 +1,13 @@
+freeStyleJob('example') {
+    
+    scm {
+        git('https://github.com/mailforsachin/com.mybank.online.git', 'master')
+    }
+  
+    steps {
+        steps {
+        shell('python test.py')
+    }
+    }
+   
+}
